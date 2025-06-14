a0bd0fd8eae88b857c3f82613067a86a0a966afa
Chilly167 <81375821+Chilly167@users.noreply.github.com>
2025-06-14 12:14:02 +0800

    Update zxUpdateManager.m

--- src/zxUpdateManager.m
+++ src/zxUpdateManager.m
@@ -1,94 +1,105 @@
 #import "zxUpdateManager.h"
+#import <os/log.h>
+#import <UIKit/UIKit.h>
 
 @implementation zxUpdateManager
+
 + (void)validityCheck {
-    // iirc some apps dont have CFBundleShortVersionString
     if ([[NSUserDefaults standardUserDefaults] boolForKey:@"zxAvoidUpdates"]) return;
 
-    NSString *version = [NSBundle.mainBundle.infoDictionary objectForKey:@"CFBundleShortVersionString"];
-    if (version == nil) return
-        [self markInvalidWithMsg:@"this app does not contain CFBundleShortVersionString and is incompatible with zxUpdateManager."
-                            text:@"okay"];
+    NSString *version = NSBundle.mainBundle.infoDictionary[@"CFBundleShortVersionString"];
+    if (!version) {
+        [self markInvalidWithMsg:@"This app does not contain CFBundleShortVersionString and is incompatible with zxUpdateManager."
+                            text:@"Okay"];
+        return;
+    }
 
-    // avoid prompting twice
     NSDictionary *latestInfo = [[NSUserDefaults standardUserDefaults] objectForKey:@"zxAppInfo"];
-    if (latestInfo == nil || ![latestInfo[@"lastSeen"] isEqualToString:version]) {
-        NSLog(@"[zxUpdateManager] checking for updates with version: %@", version);
+    if (!latestInfo || ![latestInfo[@"lastSeen"] isEqualToString:version]) {
+        os_log(OS_LOG_DEFAULT, "[zxUpdateManager] Checking for updates with version: %@", version);
         [self getAppInfoWithBundleId:NSBundle.mainBundle.bundleIdentifier currentVersion:version];
     }
 }
 
 + (void)getAppInfoWithBundleId:(NSString *)bundleId currentVersion:(NSString *)cVersion {
-    // a random param (`hi`) is needed to avoid getting a cached response
-    NSString *reqURL =
-        [NSString stringWithFormat:@"https://itunes.apple.com/lookup?limit=1&hi=%@&bundleId=%@", NSUUID.UUID, bundleId];
+    NSString *reqURL = [NSString stringWithFormat:
+        @"https://itunes.apple.com/lookup?limit=1&hi=%@&bundleId=%@",
+        NSUUID.UUID.UUIDString, bundleId];
+
     NSURLRequest *req = [NSURLRequest requestWithURL:[NSURL URLWithString:reqURL]];
 
-    [[[NSURLSession sharedSession] dataTaskWithRequest:req completionHandler:^(NSData *data, id httpResp, NSError *error) {
-        if (error) return
-            [self notifyWithMsg:[NSString stringWithFormat:@"error checking for updates:\n%@", error]
-                     buttonText:@"strange"
+    [[[NSURLSession sharedSession] dataTaskWithRequest:req
+                                     completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+        if (error) {
+            [self notifyWithMsg:[NSString stringWithFormat:@"Error checking for updates:\n%@", error.localizedDescription]
+                     buttonText:@"Strange"
                         handler:nil];
+            return;
+        }
 
         NSDictionary *resp = [NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
 
-        if ([resp[@"resultCount"] isEqual:@0]) return
-            [self markInvalidWithMsg:@"this app was not found on the app store.\n\np.s. did you change the bundle id?"
-                                text:@"okay"];
+        if ([resp[@"resultCount"] isEqual:@0]) {
+            [self markInvalidWithMsg:@"This app was not found on the App Store.\n\nP.S. Did you change the bundle ID?"
+                                text:@"Okay"];
+            return;
+        }
+
+        NSDictionary *latest = resp[@"results"][0];
+        NSString *latestVersion = latest[@"version"];
+        NSString *trackId = [latest[@"trackId"] stringValue];
 
         NSDictionary *latestInfo = @{
-            @"id": resp[@"results"][0][@"trackId"],
-            @"version": resp[@"results"][0][@"version"],
-            @"lastSeen": cVersion  // used for preventing dupe notifs
+            @"id": trackId,
+            @"version": latestVersion,
+            @"lastSeen": cVersion
         };
 
-        NSLog(@"[zxUpdateManager] latestInfo: %@", latestInfo);
-        if (![latestInfo[@"version"] isEqualToString:cVersion]) {
+        os_log(OS_LOG_DEFAULT, "[zxUpdateManager] Latest info: %@", latestInfo);
+
+        if (![latestVersion isEqualToString:cVersion]) {
             [[NSUserDefaults standardUserDefaults] setObject:latestInfo forKey:@"zxAppInfo"];
 
-            NSString
-                *updMsg =
-                    [NSString stringWithFormat:@"an update is available!\n\nv%@ -> v%@", cVersion, latestInfo[@"version"]],
-                *storeLink =
-                    [NSString stringWithFormat:@"https://apps.apple.com/app/id%@", latestInfo[@"id"]];
+            NSString *updMsg = [NSString stringWithFormat:@"An update is available!\n\nv%@ → v%@", cVersion, latestVersion];
+            NSString *storeLink = [NSString stringWithFormat:@"https://apps.apple.com/app/id%@", trackId];
 
             [self notifyWithMsg:updMsg
-                     buttonText:@"let me see!"
+                     buttonText:@"Let me see!"
                         handler:^(UIAlertAction *action) {
-                                  [[UIApplication sharedApplication]
-                                      openURL:[NSURL URLWithString:storeLink]
-                                      options:@{}
-                            completionHandler:nil];
-                        }];
+                [[UIApplication sharedApplication] openURL:[NSURL URLWithString:storeLink]
+                                                   options:@{}
+                                         completionHandler:nil];
+            }];
         }
     }] resume];
 }
 
 + (void)markInvalidWithMsg:(NSString *)msg text:(NSString *)text {
     [[NSUserDefaults standardUserDefaults] setBool:YES forKey:@"zxAvoidUpdates"];
-    return [self notifyWithMsg:msg
-                    buttonText:text
-                       handler:nil];
+    [self notifyWithMsg:msg buttonText:text handler:nil];
 }
 
 + (void)notifyWithMsg:(NSString *)msg buttonText:(NSString *)bText handler:(void (^)(UIAlertAction *action))handler {
-    NSLog(@"[zxUpdateManager] making popup with msg: %@", msg);
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2.0f * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        UIAlertController *alert =
-            [UIAlertController alertControllerWithTitle:@"zxUpdateNotifier"
-                                                message:msg
-                                         preferredStyle:UIAlertControllerStyleAlert];
+    os_log(OS_LOG_DEFAULT, "[zxUpdateManager] Displaying popup: %@", msg);
 
-        UIAlertAction *act =
-            [UIAlertAction actionWithTitle:bText
-                                     style:UIAlertActionStyleDefault
-                                   handler:handler];
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2.0f * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"zxUpdateNotifier"
+                                                                       message:msg
+                                                                preferredStyle:UIAlertControllerStyleAlert];
+
+        UIAlertAction *act = [UIAlertAction actionWithTitle:bText
+                                                      style:UIAlertActionStyleDefault
+                                                    handler:handler];
 
         [alert addAction:act];
 
-        UIViewController *rvc = UIApplication.sharedApplication.keyWindow.rootViewController;
-        while (rvc.presentedViewController) rvc = rvc.presentedViewController;
+        UIWindow *keyWindow = UIApplication.sharedApplication.windows.firstObject;
+        UIViewController *rvc = keyWindow.rootViewController;
+        while (rvc.presentedViewController) {
+            rvc = rvc.presentedViewController;
+        }
         [rvc presentViewController:alert animated:YES completion:nil];
     });
 }
+
 @end
