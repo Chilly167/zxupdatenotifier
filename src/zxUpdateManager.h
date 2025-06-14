5831be5d87216f01a5d82d2e316c01f8b7c062b7
Chilly167 <81375821+Chilly167@users.noreply.github.com>
2025-06-14 12:15:37 +0800

    Update zxUpdateManager.h

--- src/zxUpdateManager.h
+++ src/zxUpdateManager.h
@@ -1,8 +1,22 @@
+#import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 
+NS_ASSUME_NONNULL_BEGIN
+
 @interface zxUpdateManager : NSObject
+
 + (void)validityCheck;
-+ (void)getAppInfoWithBundleId:(NSString *)bundleId currentVersion:(NSString *)cVersion;
-+ (void)markInvalidWithMsg:(NSString *)msg text:(NSString *)text;
-+ (void)notifyWithMsg:(NSString *)msg buttonText:(NSString *)bText handler:(void (^)(UIAlertAction *action))handler;
+
++ (void)getAppInfoWithBundleId:(NSString *)bundleId
+               currentVersion:(NSString *)cVersion;
+
++ (void)markInvalidWithMsg:(NSString *)msg
+                      text:(NSString *)text;
+
++ (void)notifyWithMsg:(NSString *)msg
+           buttonText:(NSString *)bText
+              handler:(nullable void (^)(UIAlertAction *action))handler;
+
 @end
+
+NS_ASSUME_NONNULL_END
