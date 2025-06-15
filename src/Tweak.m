8db104f4c4bc121884a2201bf0a9f985d9fbc713
Chilly167 <81375821+Chilly167@users.noreply.github.com>
2025-06-15 11:51:34 +0800

    Update Tweak.m

--- src/Tweak.m
+++ src/Tweak.m
@@ -1,11 +1,10 @@
 #import "zxUpdateManager.h"
 
-__attribute__((constructor)) static void zxUpdateNotifierInit() {
+__attribute__((constructor)) static void init() {
     @autoreleasepool {
-        NSLog(@"[zxUpdateNotifier] Scheduling validityCheck...");
-
-        // Slight delay to ensure UIApplication is ready
-        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        NSLog(@"[zxUpdateNotifier] calling validityCheck");
+        dispatch_async(dispatch_get_main_queue(), ^{
             [zxUpdateManager validityCheck];
         });
     }
+}
